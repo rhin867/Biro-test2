c60916002a0992de7a1ab62a48794c8f99930aec
Nc <nc3937113@gmail.com>
2026-03-04 21:02:09 +0530

    Remove PWA plugin from Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Biro-Test2',
-        short_name: 'BiroTest',
-        description: 'Advanced JEE CBT Test & Analysis Platform',
-        theme_color: '#2563eb',
-        background_color: '#ffffff',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        maximumFileSizeToCacheInBytes: 15 * 1024 * 1024,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.anthropic\.com/,
-            handler: 'NetworkOnly'
-          }
-        ]
-      }
-    })
+    react()
   ],
   optimizeDeps: {
     include: ['pdfjs-dist']
